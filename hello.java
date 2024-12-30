cc63df4d2aba55bc79c6ddbfb61f2f46980791fb
rutu@2001 <rguldagad64@gmail.com>
2024-12-30 11:36:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,20 @@
  public class hello { 
 public static void main(String[] args) { 
 System.out.println("Hello, World!"); 
+ //$ cat /etc/os-release
+//Java –version
+//first copy
+//second copy
+//third first line copy
+//sudo apt-get update
+//Sudo apt.get install Jenkins
+//sudo apt.get install Jenkins
+// 1)error: sudo systemctl start Jenkins
+//2)error: sudo systemctl ststus Jenkins
+//3) sudo more (Paste)
+//4)copy id=paste crome
+
+
+
 } 
 }
